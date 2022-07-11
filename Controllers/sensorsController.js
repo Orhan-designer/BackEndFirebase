6b98291238c27f6dc06390dfed470aa92676ea09
Orhan <just.hanmamedov@gmail.com>
2022-07-11 17:36:27 +0300

    fix: API(update) for sensors device id

--- Controllers/sensorsController.js
+++ Controllers/sensorsController.js
@@ -19,60 +19,25 @@ exports.getSensors = (req, res) => {
 
 //post
 exports.createDeviceId = (req, res) => {
+  console.log(req.body)
 
-  const sensorsSelect =
-    "SELECT `ID`, `Coops_ID`, `Position`, `Device_ID`, `Type` FROM `sensors` WHERE `Device_ID` = '" +
-    req.body.deviceId +
-    "'";
+  let results = [];
+  for (let i = 0; i < req.body.length; i++) {
 
-  db.query(sensorsSelect, (error, sensorsResults) => {
-    if (sensorsResults.length) {
-      console.log('res in if', sensorsResults)
-      res.status(400).send({ message: error || 'That device id name already used.' });
-    } else {
-      const sensors =
-        "UPDATE `sensors` SET `Device_ID` = '" +
-        req.body.deviceId +
-        "' WHERE `Coops_ID` = '" +
-        req.body.id +
-        "'";
+    const sensors =
+      "UPDATE `sensors` SET `Device_ID` = '" +
+      req.body[i].Device_ID +
+      "' WHERE `ID` = '" +
+      req.body[i].ID +
+      "'";
 
-      db.query(sensors, (error, results) => {
-        if (error) {
-          res
-            .status(400)
-            .send({ message: error });
-        } else {
-          res
-            .status(200)
-            .send({ result: results });
-        }
-      });
-    }
-    // if (sensorsResults.length) {
-    //   console.log('res in if', req.body)
-    //   res.status(400).send({ message: error || 'That device id name already used.' });
-    // } else {
-    //   for (let i = 1; i < req.body.length; i++) {
-    //     const sensors =
-    //       "UPDATE `sensors` SET `Device_ID` = '" +
-    //       req.body[i].Device_ID +
-    //       "' WHERE `Coops_ID` = '" +
-    //       req.body.id +
-    //       "'";
-
-    //     db.query(sensors, (error, results) => {
-    //       if (error) {
-    //         res
-    //           .status(400)
-    //           .send({ message: error });
-    //       } else {
-    //         console.log(results)
-    //         sensorsResults[i] = results;
-    //       }
-    //     });
-    //     res.status(200).send({ result: sensorsResults });
-    //   }
-    // }
-  });
+    db.query(sensors, (error, sensorsResults) => {
+      if (sensorsResults.length) {
+        res.status(400).send({ message: error || 'That device id name already used.' });
+      } else {
+        results.push(sensorsResults)
+      }
+    });
+  }
+  res.status(200).send({ result: results })
 };
